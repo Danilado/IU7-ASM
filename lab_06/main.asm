d27778bf9d8de9eb8592c13998594c8c6b868583
Danilado <danya.zvyagin@gmail.com>
2024-04-13 14:53:19 +0300

    lab_06 works now

--- lab_06/main.asm
+++ lab_06/main.asm
@@ -2,31 +2,31 @@
 
 CSEG SEGMENT 
     assume CS:CSEG
-    org 100h            
+    org 100h
 
     main:
         jmp init
-        
+
         speed db 00011111b
         flag db 42
-        cur_time db 0
+        timestamp db 0
         old_handler dd 0
 
-    handler proc near
+    inter proc near
         pushf
-        ; call CS:old_handler
+        call cs:old_handler
 
         mov ah, 02h ; CH:CL:DH
         int 1ah
 
-        cmp dh, cur_time    
-        je no_change_speed  
+        cmp dh, timestamp
+        je no_change_speed
 
-        mov cur_time, dh    
-        dec speed           
+        mov timestamp, dh
+        dec speed
 
         mov al, 0F3h
-        out 60h, al 
+        out 60h, al
         mov al, speed
         out 60h, al
 
@@ -37,58 +37,53 @@ CSEG SEGMENT
 
         no_change_speed:
 
-        popf
         iret
-    HANDLER endp
+    inter endp
 
-    init:       
+    init:
         mov ax, 351ch ; предыдущее прерывание
         int 21h
 
         cmp es:flag, 42   
         je remove                  
 
-        mov word ptr old_handler, bx  
-        mov word ptr old_handler + 2, es    
+        mov word ptr old_handler, bx
+        mov word ptr old_handler + 2, es
 
-        mov ax, 251ch              
-        mov dx, offset HANDLER
-        int 21h                     
+        mov ax, 251ch
+        mov dx, offset inter
+        int 21h
 
-        mov dx, offset init_msg     
+        mov dx, offset init_msg
         mov ah, 09h
-        int 21h                            
+        int 21h
 
-        mov ah, 27h                         
+        mov ah, 27h
         mov dx, offset init ; init - последий байт программы (не обработчик прерывашки)
         int 27h             ; Делаем резидентом
 
     remove:
-        mov dx, offset exit_msg             
+        mov dx, offset exit_msg
         mov ah, 09h
         int 21h
 
-        mov al, 0F3h    
-
-        out 60h, al    
-
-        ;устанавливаем период автоповтора 30.0, задержку включения режима автоповтора 250 мс (восстанавливаем дефолтные значения)
+        mov al, 0F3h
+        out 60h, al
         mov al, 0
-        out 60h, al                 
-        
-        ; устанавливаем период автоповтора 30.0, задержку включения режима автоповтора 250 мс (восстанавливаем дефолтные значения)
-        mov dx, word ptr es:old_handler                       
+        out 60h, al
+
+        mov dx, word ptr es:old_handler
         mov ds, word ptr es:old_handler + 2
         mov ax, 251ch
-        int 21h                  
+        int 21h
 
-        mov ah, 49h                         
+        mov ah, 49h
         int 21h
 
         mov ax, 4c00h
         int 21h
 
-        init_msg db "lab_06 init", 10, 13, '$'   
+        init_msg db "lab_06 init", 10, 13, '$'
         exit_msg db "lab_06 stop", 10, 13, '$'
 CSEG ENDS
 END MAIN
